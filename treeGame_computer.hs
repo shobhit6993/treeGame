2a16e0a6b40c097c535814735ac9f3be4b0cc94a
Shobhit Chaurasia <sc.shobhit@gmail.com>
2014-10-28 23:44:13 +0530

    AI implemented. Code refactored

--- treeGame_computer.hs
+++ treeGame_computer.hs
@@ -3,10 +3,12 @@ import Data.Fixed
 import Data.List.Split
 import Control.Monad (forM_, liftM)
 
-player = ["X","Y"]
+player = ["U","C"]
 colour = ['R','G']
 
--- Player X can remove Red or Blue; Player Y can remove Green or Blue
+-- User plays first. Then Computer plays
+-- Player C is Computer. Player U is user.
+-- Player U can remove Red or Blue; Player C can remove Green or Blue
 -- Levels, Trees, and Nodes are indexed starting from ONE (1)
 
 data Node = Node 
@@ -121,23 +123,54 @@ main = do
 gameLoop:: State->Int->IO ()
 gameLoop state id = do
 	display state
-	if isFinished state
-		then putStrLn ("Player "++(player !! id)++" lost :(")
+	if ((isFinished state) || (compMove == []))
+		then putStrLn ("\n!!!!!!!!!!!!!!!!!!!!!!!!Player "++(player !! id)++" lost!!!!!!!!!!!!!!!!!!!!!!!!\n")
 	else do
-		move <- getInput
-		if isValid move state id 	--check if given move is valid for curr player on curr state
-			then gameLoop (modify state move) (mod' (id+1) 2)
-						-- gameLoop with modified state and next player
-		else
-			do
-				putStrLn ("Invalid Move! Try again...")
-				gameLoop state id 	--if invalid move, then gameLoop with same state and same player
+		putStrLn ("\n------------------------Player "++(player !! id)++" turn --------------------------\n")
+		if(id==1)		--computer's move
+			then
+				if (compMove == [])
+					then putStrLn ("\n!!!!!!!!!!!!!!!!!!!!!!!!Player "++(player !! id)++" lost!!!!!!!!!!!!!!!!!!!!!!!!\n")
+				else
+					gameLoop (modify state compMove) (mod' (id+1) 2)
+				
 
+		else do			--user's move
+			move <- getInput
+			if isValid move state id 	--check if given move is valid for curr player on curr state
+				then gameLoop (modify state move) (mod' (id+1) 2)
+							-- gameLoop with modified state and next player
+			else
+				do
+					putStrLn ("Invalid Move! Try again...")
+					gameLoop state id 	--if invalid move, then gameLoop with same state and same player
+	where 
+		compMove = computerMove state id
+--------------------------------------------------------------
 
 -- functions for the AI part
 
+computerMove :: State->Int->Move
+computerMove state id =
+	if fst'(move) == False then
+		if null randomEdge
+			then []
+		else [fst'(randomEdge !! 0), snd'(randomEdge !! 0),thrd'(randomEdge !! 0)]
+	else
+		[snd'(move),thrd'(move),frth'(move)]
+
+	where
+		edges = getAllEdges state 0
+		move = winOnOne edges edges id
+		fst' (f,_,_,_) = f
+		snd' (_,s,_,_) = s
+		thrd' (_,_,t,_) = t
+		frth' (_,_,_,f) = f
+		randomEdge = (filter (\e -> (frth'(e)==(colour !! id)) || (frth'(e)=='B')) edges)
+
+
 --returns the combined edgeList of ALL trees as a list of 4 tuple (tId, sourceId, DestId, EdgeColour)
---intially pass idx=0
+--intially pass idx as 0
 getAllEdges :: State->Int->[(Int,Int,Int,Char)]
 getAllEdges (tree:[]) idx = getEdgesOneTree tree idx
 getAllEdges (tree:xs) idx = (getEdgesOneTree tree idx)++(getAllEdges xs (idx+1))
@@ -154,8 +187,9 @@ edgeList tId s childList = zipWith (\x y -> (tId, s, fst(y), snd(y))) [s..] chil
 --given the combined edges of ALL trees, it returns an edge removing which is a winning strategy for player#id,
 --if no such edge exists, an invalid edge
 -- edge is represented as the 4-tuple (Bool, tId, sourceId, destId) where bool = false means invalid edge.
-temp :: [(Int,Int,Int,Char)]
-temp = [(1,1,2,'R'),(1,2,3,'G'),(1,3,4,'R'),(2,1,2,'R'),(3,1,2,'G')]
+temp :: State
+temp = [[Node {nodeId = 1, adjList = []}],[Node {nodeId = 1, adjList = [(3,'G')]},Node {nodeId = 3, adjList = []}]]
+--temp  [(1,1,2,'R'),(1,2,3,'G'),(1,3,4,'R'),(2,1,2,'R'),(3,1,2,'G')]
 
 winOnOne :: [(Int,Int,Int,Char)]->[(Int,Int,Int,Char)]->Int->(Bool, Int, Int, Int)
 winOnOne allEdges [] pId = (False, -1, -1, -1)
@@ -253,6 +287,7 @@ loadState levelNo = levels !! levelNo
 					]
 
 -----------------------------------------------
+
 {- Basic Code Flow
 
 	load state -- initial state
