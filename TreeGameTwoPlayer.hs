759a761dc257b78a94edf0fd81f4679a908bf3f7
Shobhit Chaurasia <sc.shobhit@gmail.com>
2014-10-30 21:12:19 +0530

    GUI integrated with 2 player

---
+++ TreeGameTwoPlayer.hs
@@ -0,0 +1,401 @@
+module TreeGameTwoPlayer where
+import Data.Fixed
+import Data.List.Split
+import Control.Monad (forM_, liftM)
+
+player = ["X","Y"]
+colour = ['R','G']
+
+-- Player X can remove Red or Blue; Player Y can remove Green or Blue
+-- Levels, Trees, and Nodes are indexed starting from ONE (1)
+
+initgs :: Int->Int->GameState
+initgs levelId pId = 
+	GameState {turn = (if(pId==0) then True else False), treeList=state}
+	where state = loadState levelId
+
+
+convertCol :: Char->Int
+convertCol charColour = if charColour == 'R' then 1
+							else if charColour == 'G' then 3
+								else 2
+
+formatConversion :: [(Int, Char)]->[(Int, Int)]
+formatConversion adjlist = map (\dest -> ((fst dest), (convertCol (snd dest)) )) adjlist
+
+getDestList :: Tree->Int->[(Int, Int)]
+getDestList [] src = []
+getDestList (node:nodelist) src = 
+	if (nodeId node) == src 
+		then (formatConversion (adjList node)) 
+	else getDestList nodelist src
+
+getPlayerID :: Bool->Int
+getPlayerID (turn) = if turn then 0 else 1
+
+--playMove :: (Int,Int,Int) -> IORef GameState -> IORef GameState
+playMove (treeNum, srcNum, destNum) gs = GameState {turn = not (turn gs), treeList = newState}
+	where
+		newState = (modify (treeList gs) [treeNum, srcNum, destNum])
+
+playAIMove = undefined
+data GameState = GameState
+				{
+					turn :: Bool
+					,treeList :: [Tree]
+				}
+
+data Node = Node 
+			{
+				nodeId :: Int
+				,adjList :: [(Int,Char)]
+			}	deriving (Show)
+
+type Move = [Int] -- (tree no., source node id, dest node id). Size must be THREE (3)
+type State = [Tree]
+type Tree = [Node]
+
+
+--display :: State -> IO ()
+--display state = print state
+
+-- game is finished when each tree has only the root node left, with no edges
+-- so each Tree of State should have EXACTLY one Node
+isFinished :: State -> Bool
+isFinished state = null nonEmptyTrees
+	where
+		nonEmptyTrees = filter (\tree -> (length tree) > 1) state
+
+getLevelNo :: IO Int
+getLevelNo = do
+	input <- getLine
+	return (read input :: Int)
+
+getInput :: IO Move
+getInput = do
+	input <- getLine
+	let tempMove = splitOn "," input
+	let move = map (\x -> (read x :: Int)) tempMove
+	return move
+
+-- Load all levels from file. Implement later
+loadAllLevels :: String -> IO [State]
+loadAllLevels filename = undefined
+	--lns <- liftM lines . readFile $ filename
+	--return lns
+	--return $ unfoldr consume lns
+	--where isEmptyLine = all (' '==)
+	--      consume [] = Nothing
+	--      consume ls = let (a,b) = break isEmptyLine ls
+	--                   in return (loadLevel $ unlines a, drop 1 b)
+
+isValid :: Move->State->Int->Bool
+isValid move state id =
+	if length move /= 3
+		then False
+	else
+		if ((length edge == 0) || (t==0))
+			then False
+		else
+			(snd (edge !! 0) == (colour !! id)) || (snd (edge !! 0) == 'B')
+
+	where
+		t = move !! 0
+		s = move !! 1
+		d = move !! 2
+		tree = (state !! t)
+		filteredTree = filter (\x -> (nodeId x)==s) tree
+		srcAdjList = if length filteredTree==0 then [] else adjList (filteredTree !! 0)
+		edge = filter (\x -> (fst x)==d) srcAdjList
+
+
+pruneSubTree :: Int->Tree->Tree
+pruneSubTree s tree = 
+	cleanList (map (\c -> if((nodeId c) == s) then Node{nodeId = -1,adjList = [(-1,'N')]} else c) tree')
+	where
+		children = getChildren tree s
+		tree' = foldl (\acc c -> if c == -1 then acc else (pruneSubTree c acc)) tree children
+
+getChildren :: Tree->Int->[Int]
+getChildren tree s = 
+	map(\t -> fst(t)) adjListOfs
+	where
+	adjListOfs 	= adjList (tempList !! 0)
+	tempList = filter (\x -> (nodeId x) == s) tree
+
+cleanList :: Tree->Tree
+cleanList tree = 
+	foldl (\acc x -> if ((nodeId x) == -1) then acc else [x]++acc) [] tree
+
+modify :: State->Move->State
+modify state [] = state 		--if move is empty, return unchanged state
+modify state move = 
+	take t state ++ [(dfs tree move)] ++ drop (t + 1) state
+	where
+		t = move !! 0
+		tree = (state !! t)
+
+dfs :: Tree->Move->Tree
+dfs tree move = 
+	pruneSubTree d newTree
+	where
+		s = move !! 1
+		d = move !! 2
+		srcAdjList = adjList ((filter (\x -> (nodeId x)==s) tree) !! 0)
+		newAdjList = foldl (\acc x -> if fst(x)==d then acc else [x]++acc) [] srcAdjList
+		newTree = 
+			foldl (\acc x ->	if (nodeId x)==s 
+									then [Node {nodeId=s, adjList=newAdjList}] ++ acc
+								else [x]++acc) [] tree
+
+--main = do
+--	--levels <- loadAllLevels "levels.txt"
+--	levelNo <- getLevelNo
+--	--let state = levels !! levelNo
+--	let state = loadState levelNo
+--	gameLoop state 0	-- call gameLoop with initial state and first player
+--	return ()
+
+gameLoop:: State->Int->IO ()
+gameLoop state id = do
+	--display state
+	if ((isFinished state) || (prediction == []))
+		then putStrLn ("\n!!!!!!!!!!!!!!!!!!!!!!!!Player "++(player !! id)++" lost!!!!!!!!!!!!!!!!!!!!!!!!\n")
+	else do
+		putStrLn ("\n------------------------Player "++(player !! id)++" turn --------------------------\n")
+		move <- getInput
+		if isValid move state id 	--check if given move is valid for curr player on curr state
+			then gameLoop (modify state move) (mod' (id+1) 2)
+						-- gameLoop with modified state and next player
+		else
+			do
+				putStrLn ("\nInvalid Move! Try again...")
+				gameLoop state id 	--if invalid move, then gameLoop with same state and same player
+
+	where 
+		prediction = computerMove state id
+
+--------------------------------------------------------------
+
+-- functions for the AI part used here just to predict if game ended or not
+-- FOR UNDERSTANDIG THIS, ITS BETTER TO READ THE computer VERSION OF THE GAME in treeGame_computer.hs
+
+computerMove :: State->Int->Move
+computerMove state id =
+	if fst'(move) == False then
+		if null randomEdge
+			then []
+		else [fst'(randomEdge !! 0), snd'(randomEdge !! 0),thrd'(randomEdge !! 0)]
+	else
+		[snd'(move),thrd'(move),frth'(move)]
+
+	where
+		edges = getAllEdges state 0
+		move = winOnOne edges edges id
+		fst' (f,_,_,_) = f
+		snd' (_,s,_,_) = s
+		thrd' (_,_,t,_) = t
+		frth' (_,_,_,f) = f
+		randomEdge = (filter (\e -> (frth'(e)==(colour !! id)) || (frth'(e)=='B')) edges)
+
+
+--returns the combined edgeList of ALL trees as a list of 4 tuple (tId, sourceId, DestId, EdgeColour)
+--intially pass idx as 0
+getAllEdges :: State->Int->[(Int,Int,Int,Char)]
+getAllEdges (tree:[]) idx = getEdgesOneTree tree idx
+getAllEdges (tree:xs) idx = (getEdgesOneTree tree idx)++(getAllEdges xs (idx+1))
+
+--returns the edgeList of tree#tId as a list of 4 tuple (tId, sourceId, DestId, EdgeColour)
+getEdgesOneTree :: Tree->Int->[(Int,Int,Int,Char)]
+getEdgesOneTree tree tId = 
+	foldl (\acc n -> if((length (adjList n)) == 0) then acc else (edgeList tId (nodeId n) (adjList n)) ++acc) [] tree
+
+--returns the edgeList of for a particular source of tree#tId as a list of 4 tuple (tId, sourceId, DestId, EdgeColour)
+edgeList :: Int->Int->[(Int, Char)]->[(Int,Int,Int,Char)]
+edgeList tId s childList = zipWith (\x y -> (tId, s, fst(y), snd(y))) [s..] childList
+
+--given the combined edges of ALL trees, it returns an edge removing which is a winning strategy for player#id,
+--if no such edge exists, an invalid edge
+-- edge is represented as the 4-tuple (Bool, tId, sourceId, destId) where bool = false means invalid edge.
+temp :: State
+temp = [[Node {nodeId = 1, adjList = []}],[Node {nodeId = 1, adjList = [(3,'G')]},Node {nodeId = 3, adjList = []}]]
+--temp  [(1,1,2,'R'),(1,2,3,'G'),(1,3,4,'R'),(2,1,2,'R'),(3,1,2,'G')]
+
+winOnOne :: [(Int,Int,Int,Char)]->[(Int,Int,Int,Char)]->Int->(Bool, Int, Int, Int)
+winOnOne allEdges [] pId = (False, -1, -1, -1)
+winOnOne allEdges (e:edges) pId = 
+	if ((frth'(e)==(colour !! pId) || frth'(e)=='B') && (looseOnAll allEdges' allEdges' (mod' (pId+1) 2)) == True)
+		then
+			(True, fst'(e), snd'(e), thrd'(e))
+		else
+			winOnOne allEdges edges pId
+	where
+		fst' (f,_,_,_) = f
+		snd' (_,s,_,_) = s
+		thrd' (_,_,t,_) = t
+		frth' (_,_,_,f) = f
+		allEdges' = 
+			foldl (\acc x ->if fst'(x)==fst'(e) && snd'(x)==snd'(e) && thrd'(x)==thrd'(e) && frth'(x)==frth'(e)
+								then acc
+							else [x]++acc) [] allEdges
+
+--given the combined edges of ALL trees, it returns True if there does not exist any winning strategy for player#pId in the form of removal of an edge 
+--if atleast one such edge exists, it returns False
+looseOnAll :: [(Int,Int,Int,Char)]->[(Int,Int,Int,Char)]->Int->Bool
+looseOnAll allEdges [] pId = True
+looseOnAll allEdges (e:edges) pId = 
+	if ((frth'(e)==(colour !! pId) || frth'(e)=='B'))
+		then
+			if(fst'(winOnOne allEdges' allEdges' (mod' (pId+1) 2)) == False)
+				then
+					False
+				else
+					looseOnAll allEdges edges pId
+		else
+			looseOnAll allEdges edges pId
+	where
+		fst' (f,_,_,_) = f
+		snd' (_,s,_,_) = s
+		thrd' (_,t,_,_) = t
+		frth' (_,_,_,f) = f
+		allEdges' = 
+			foldl (\acc x ->if fst'(x)==fst'(e) && snd'(x)==snd'(e) && thrd'(x)==thrd'(e) && frth'(x)==frth'(e)
+								then acc
+							else [x]++acc) [] allEdges
+
+-------------------------------------------------------
+
+loadState :: Int->State
+loadState levelNo = levels !! levelNo
+	where levels =	[
+						[ 
+							[
+								Node {nodeId = 1, adjList = []}
+							]
+						],
+
+						--1
+						[
+							[
+								Node {nodeId = 1, adjList = []}
+							] , 
+							
+							[
+								Node {nodeId=1, adjList=[(2,'R'),(3,'G'),(4,'B')]},
+								Node {nodeId=2, adjList=[(5,'B')]},
+								Node {nodeId=3, adjList=[]},
+								Node {nodeId=4, adjList=[(6,'G')]},
+								Node {nodeId=5, adjList=[]},
+								Node {nodeId=6, adjList=[(7,'G'),(8,'R')]},
+								Node {nodeId=7, adjList=[]},
+								Node {nodeId=8, adjList=[]}
+							] 
+						],
+
+						--2
+						[
+							[
+								Node {nodeId = 1, adjList = []}
+							],
+
+							[
+								Node {nodeId=1, adjList=[(2,'R'),(3,'G'),(4,'B')]},
+								Node {nodeId=2, adjList=[(5,'B')]},
+								Node {nodeId=3, adjList=[]},
+								Node {nodeId=4, adjList=[(6,'G')]},
+								Node {nodeId=5, adjList=[]},
+								Node {nodeId=6, adjList=[(7,'G'),(8,'R')]},
+								Node {nodeId=7, adjList=[]},
+								Node {nodeId=8, adjList=[]}
+							],
+
+							[
+								Node {nodeId=1, adjList=[(2,'R')]},
+								Node {nodeId=2, adjList=[(3,'G')]},
+								Node {nodeId=3, adjList=[]}
+							]
+
+						],
+
+						--3
+						[
+							[
+								Node {nodeId = 1, adjList = []}
+							],
+
+							[
+								Node {nodeId=1, adjList=[(2,'R')]},
+								Node {nodeId=2, adjList=[(3,'G')]},
+								Node {nodeId=3, adjList=[(4,'R')]},
+								Node {nodeId=4, adjList=[]}
+							],
+
+							[
+								Node {nodeId=1, adjList=[(2,'R')]},
+								Node {nodeId=2, adjList=[]}
+							],
+
+							[
+								Node {nodeId=1, adjList=[(2,'G')]},
+								Node {nodeId=2, adjList=[]}
+							]
+
+						]
+					]
+
+-----------------------------------------------
+{- Basic Code Flow
+
+	load state -- initial state
+	display state
+
+	while not isFinished
+	{
+		get input from Player1
+		gameStep input
+		{
+			if isValid input -- print error message inside isValid fn.
+				modify state
+		}
+		display state
+	
+		if isFinished
+			break
+
+		get input from Player2
+		gameStep input
+		{
+			if isValid input
+				modify state
+		}
+		display state
+	}
+-}
+
+
+{- sampleTree 
+		= [
+			Node {nodeId=1, adjList=[(2,'R'),(3,'G'),(4,'B')]},
+			Node {nodeId=2, adjList=[(5,'B')]},
+			Node {nodeId=3, adjList=[]},
+			Node {nodeId=4, adjList=[(6,'G')]},
+			Node {nodeId=5, adjList=[]},
+			Node {nodeId=6, adjList=[(7,'G'),(8,'R')]},
+			Node {nodeId=7, adjList=[]},
+			Node {nodeId=8, adjList=[]}
+		] 
+
+			1
+		  / | \
+		 r  g  b
+		/	|   \
+	   2	3	4
+	  /			\
+	 b 		  	g
+	/			\
+   5 			6
+	   		   / \	   
+	   		  g  r
+	   		 /    \
+	   		7	   8
+-}
---
+++ gui_ai.hs
@@ -0,0 +1,301 @@
+import System.Environment(getArgs)
+import System.Random
+import Control.Monad.Trans
+import Control.Monad.IO.Class
+import Graphics.UI.GLUT
+import Data.IORef
+import Data.Char
+import Control.Monad
+import TreeGameTwoPlayer
+import Control.Monad.State.Lazy
+import qualified Foreign.C.Types
+
+type NDouble = Foreign.C.Types.CDouble
+
+main = do
+	initialWindowSize $= Size 1300 700
+	initialDisplayMode $= [DoubleBuffered]
+	(_progName, _args) <- getArgsAndInitialize
+	_window <- createWindow "treeGame"
+	--displayCallback $= display
+	reshapeCallback $= Just reshape
+	state 		<- newIORef 0
+	treeNum 	<- newIORef 0
+	srcNum 		<- newIORef 0
+	destNum 	<- newIORef 0
+
+	args <- getArgs
+	gs 			<- newIORef (gameSt args)
+	--state 	<- get state'
+	gs' <- readIORef gs
+
+	if (read $ head args :: Int) == 1
+	then displayCallback $= display state treeNum srcNum destNum gs (points gs')
+	else displayCallback $= display' state treeNum srcNum destNum gs (points gs')
+
+	keyboardMouseCallback $= Just (keyboardMouse ((read $ head args :: Int) == 1) state treeNum srcNum destNum gs) 
+	
+	mainLoop
+	where
+
+		levelid args = (read $ head (tail args) :: Int)
+		points gs = (computeTreeListCoordinates (treeList gs) (-1.05) (-0.9) (-0.9))
+		gameSt args = initgs (levelid args) 0 
+				
+playGame :: IORef Int -> IORef Int -> IORef Int -> IORef GameState -> IO ()
+playGame treeNum' srcNum' destNum' gs' = do
+	treeNum <- readIORef treeNum'
+	srcNum <- readIORef srcNum'
+	destNum <- readIORef destNum'
+	gs <- readIORef gs'	
+	--modifyIORef gs' $ playMove (treeNum, srcNum, destNum)
+	if (isValid  [treeNum, srcNum, destNum] (treeList gs) (getPlayerID (turn gs)))
+		then modifyIORef gs' $ playMove (treeNum, srcNum, destNum)
+		else return ()
+
+
+
+drawAllTrees [] [] treeIdx = return ()
+drawAllTrees (tree:treelist) (points:pointslist) treeIdx = do
+	drawTree tree tree points treeIdx
+	drawAllTrees treelist pointslist (treeIdx + 1)
+
+display :: IORef Int -> IORef Int -> IORef Int -> IORef Int -> IORef GameState -> [[(Int, (GLfloat, GLfloat))]] -> DisplayCallback
+display state' treeNum' srcNum' destNum' gs' points = do
+	gs <- readIORef gs'
+	if (turn gs)
+	then display' state' treeNum' srcNum' destNum' gs' points
+	else display'' gs' points
+
+display' state' treeNum' srcNum' destNum' gs' points = do
+	state 	<- Graphics.UI.GLUT.get state'
+	treeNum <- Graphics.UI.GLUT.get treeNum'
+	srcNum	<- Graphics.UI.GLUT.get srcNum'
+	destNum <- Graphics.UI.GLUT.get destNum'
+	gs <- Graphics.UI.GLUT.get gs'
+	playGame treeNum' srcNum' destNum' gs'
+	clear [ColorBuffer,DepthBuffer]
+	color $ Color3 0 1 (0 :: GLfloat)
+	currentRasterPosition $= vertex4 (-0.99) (0.8) 0 1
+	renderString Fixed8By13 (displayString state treeNum srcNum destNum)
+	currentRasterPosition $= vertex4 (-0.99) (0.7) 0 1
+	renderString Fixed8By13 (displayString' state treeNum srcNum destNum)
+	--currentRasterPosition $= vertex4 (-0.75) (0.3) 0 1
+	--renderString Fixed8By13 "Jhu"
+  	--renderPrimitive Points $ mapM_ (\(x, (y, z)) -> vertex $ Vertex2 ((fromIntegral (y-2) :: GLfloat)/3) ((fromIntegral (z-2) :: GLfloat)/3)) points
+	drawAllTrees (treeList gs) points 0
+
+	if (isFinished (treeList gs))
+		then drawString (-0.99, 0.9) ("Game Over!! " ++ (currentTurn gs) ++ " wins!!")
+		else drawString (-0.99, 0.9) ((currentTurn' gs) ++ "'s turn")
+	--drawTree (tree0) (nodeList tree0) (points !! 0)
+	swapBuffers
+	where
+		displayString state treeNum srcNum destNum = case state of
+			0 -> "Enter Tree Number: "
+			1 -> "Entered Tree Number: " 		++ (show treeNum)
+			2 -> "Entered Source Number: " 		++ (show srcNum)
+			3 -> "Entered Destination Number: " ++ (show destNum)
+
+		displayString' state treeNum srcNum destNum = case state of
+			0 -> ""
+			1 -> "Enter Source Number: "
+			2 -> "Enter Destination Number: "
+			3 -> "Play Move?"
+
+		currentTurn gs
+			| (turn gs) = "Player 2"
+			| otherwise = "Player 1"
+
+		currentTurn' gs
+			| (turn gs) = "Player 1"
+			| otherwise = "Player 2"
+
+display'' gs' points = do
+	gs <- Graphics.UI.GLUT.get gs'
+	clear [ColorBuffer,DepthBuffer]
+	color $ Color3 0 1 (0 :: GLfloat)
+	modifyIORef gs' $ playAIMove
+	drawAllTrees (treeList gs) points 0
+	if (isFinished (treeList gs))
+		then drawString (-0.99, 0.9) ("Game Over!! " ++ (currentTurn gs) ++ " wins!!")
+		else drawString (-0.99, 0.9) ((currentTurn' gs) ++ "'s turn")
+	swapBuffers
+	where
+		currentTurn gs
+			| (turn gs) = "Player 2"
+			| otherwise = "Player 1"
+
+		currentTurn' gs
+			| (turn gs) = "Player 1"
+			| otherwise = "Player 2"
+
+drawLine :: (GLfloat, GLfloat) -> (GLfloat, GLfloat) -> Int -> IO ()
+drawLine (x1, y1) (x2, y2) colour = renderPrimitive Lines $ do
+						color $ getColour colour
+						vertex $ Vertex2 ((x1) :: GLfloat) ((y1) :: GLfloat)
+						vertex $ Vertex2 ((x2) :: GLfloat) ((y2) :: GLfloat)
+						where
+							getColour colour
+								| colour == 1 		= Color3 1 0 (0 :: GLfloat)
+								| colour == 2  		= Color3 0 0 (1 :: GLfloat)
+								| otherwise 		= Color3 0 1 (0 :: GLfloat) 
+
+drawString (x,y) str = do
+	color $ Color3 1 1 (1 :: GLfloat)
+	currentRasterPosition $= vertex4 ((x) :: GLfloat) ((y) ::GLfloat) 0 1
+	renderString Fixed8By13 str
+
+drawNode :: Tree -> [(Int,(GLfloat,GLfloat))] -> Int -> Int -> IO()
+drawNode tree points root treeIdx = do
+	drawEdge root dstList points
+	drawString (findCoord root points) (show root)
+	drawTreeNum root (findCoord root points) treeIdx
+	where
+		dstList = getDestList tree root
+		findCoord node (x:points) = if (node == (fst x))
+										then (snd x)
+										else findCoord node points
+		drawEdge src [] points = return ()
+		drawEdge src (dest : destList) points = do
+			if fst(dest) /= -1 then drawLine (findCoord src points) (findCoord (fst dest) points) (snd dest) else return ()
+			drawEdge src destList points 
+
+drawTreeNum root (x,y) treeIdx = if root == 1 then drawString (x-0.05,y-0.05) ("Tree " ++ (show treeIdx)) else return ()
+
+
+drawTree tree [] points treeIdx = return ()
+drawTree tree (node:nodeList) points treeIdx = do
+	drawTree tree nodeList points treeIdx
+	--print (src node)
+	if ((nodeId node) == -1) 
+		then return () 
+		else drawNode tree points (nodeId node) treeIdx
+
+
+postOrderList :: [Tree] -> Int -> Int -> [[(Int, Int)]]
+postOrderList treeList root depth =
+	foldl (\acc tree -> (postOrder tree root depth) : acc) [] treeList
+
+postOrder :: Tree -> Int -> Int -> [(Int, Int)]
+postOrder tree root depth =
+	foldl (\acc (x, _) -> (if x /= -1 then ((postOrder tree x (depth + 1)) ++ acc) else acc)) [(root, depth)] dstList
+	where
+		dstList = getDestList tree root
+
+
+plotted' :: [(Int, (GLfloat, GLfloat))] -> GLfloat -> (Int, Int) -> Tree -> GLfloat -> [(Int, (GLfloat, GLfloat))]
+plotted' plotted maxX x tree yoffset
+	| length (getDestList tree (fst x)) == 0 	= (((fst x),(maxX + 0.1, ((fromIntegral (snd x) :: GLfloat)/7) + yoffset)) :plotted)
+	| otherwise									= ((fst x,( getChildAvg tree plotted (fst x), ((fromIntegral (snd x) :: GLfloat)/7) + yoffset)):plotted) 
+
+maxX' :: GLfloat -> (Int, Int) -> Tree -> GLfloat
+maxX' maxX x tree
+	| length (getDestList tree (fst x)) == 0 	= maxX + 0.1
+	| otherwise									= maxX
+
+
+maxY' :: GLfloat -> (Int, Int) -> Tree -> GLfloat -> GLfloat
+maxY' maxY x tree yoffset
+	| (((fromIntegral (snd x) :: GLfloat)/7) + yoffset > maxY) 	= ((fromIntegral (snd x) :: GLfloat)/7) + yoffset
+	| otherwise											= maxY
+
+
+
+
+getChildAvg tree plotted src = (sumX tree plotted src) / (lenX tree plotted src)
+
+sumX :: Tree -> [(Int, (GLfloat, GLfloat))] -> Int -> GLfloat
+sumX tree plotted src = foldl(\acc x -> acc + x) 0.0 (getChildX tree plotted src)
+
+lenX :: Tree -> [(Int, (GLfloat, GLfloat))] -> Int -> GLfloat
+lenX tree plotted src = fromIntegral (length (getChildX tree plotted src)) :: GLfloat
+
+getChildX tree plotted src = map(\x -> (getX plotted (fst x))) (getDestList tree src)
+
+--get [] node = -1
+getX (x:xs) node = if fst(x) == node then (fst(snd(x))) else (getX xs node)
+
+
+
+computeTreeCoordinates :: Tree -> [(Int, Int)] -> [(Int, Int)] -> [(Int, (GLfloat, GLfloat))] -> GLfloat -> GLfloat -> GLfloat -> ([(Int, (GLfloat, GLfloat))], GLfloat, GLfloat, GLfloat)
+computeTreeCoordinates tree [] (backupPostOrder) plotted maxX maxY yoffset = if (maxX > 1.0)
+																				then computeTreeCoordinates tree backupPostOrder backupPostOrder [] (-1.1) maxY (maxY+0.1)
+																				else (plotted, maxX, yoffset, maxY)
+computeTreeCoordinates tree (x:xs) (backupPostOrder) plotted maxX maxY yoffset =
+	computeTreeCoordinates tree xs (backupPostOrder) (plotted' plotted maxX x tree yoffset) (maxX' maxX x tree) (maxY' maxY x tree yoffset) yoffset
+
+
+
+
+--getCurMaxX [] endTree
+--getCurMaxX (tree:treeList) endTree 
+
+computeTreeListCoordinates :: [Tree] -> GLfloat -> GLfloat -> GLfloat -> [[(Int, (GLfloat, GLfloat))]]
+computeTreeListCoordinates [] xoffset yoffset maxY = []
+computeTreeListCoordinates (tree:treeList) xoffset yoffset maxY = (treePoints : (computeTreeListCoordinates treeList xoffset' yoffset' maxY'))
+	where
+		(treePoints, xoffset', yoffset', maxY') = computeTreeCoordinates tree (postOrder tree 1 0) (postOrder tree 1 0) [] xoffset maxY yoffset
+
+
+
+keyboardMouse :: Bool -> IORef Int -> IORef Int -> IORef Int -> IORef Int -> IORef GameState -> KeyboardMouseCallback
+keyboardMouse isAI state treeNum srcNum destNum gs key Down _ _ = case key of
+	(Char '0') -> modifyNum '0' treeNum srcNum destNum state
+	(Char '1') -> modifyNum '1' treeNum srcNum destNum state
+	(Char '2') -> modifyNum '2' treeNum srcNum destNum state
+	(Char '3') -> modifyNum '3' treeNum srcNum destNum state
+	(Char '4') -> modifyNum '4' treeNum srcNum destNum state
+	(Char '5') -> modifyNum '5' treeNum srcNum destNum state
+	(Char '6') -> modifyNum '6' treeNum srcNum destNum state
+	(Char '7') -> modifyNum '7' treeNum srcNum destNum state
+	(Char '8') -> modifyNum '8' treeNum srcNum destNum state
+	(Char '9') -> modifyNum '9' treeNum srcNum destNum state
+	(SpecialKey KeyRight) -> if isAI
+							then modifyState state treeNum srcNum destNum
+							else modifyState' state treeNum srcNum destNum
+	_ -> return ()
+
+	where
+		modifyNum'' num key = do
+			num $~! (* 10)
+			num $~! (+ (ord key - ord '0'))
+		modifyNum key treeNum srcNum destNum state = do
+			state' <- Graphics.UI.GLUT.get state
+			modifyNum' key treeNum srcNum destNum state'
+			where
+				modifyNum' key treeNum srcNum destNum state = case state of
+					0 -> modifyNum'' treeNum key
+					1 -> modifyNum'' srcNum  key
+					2 -> modifyNum'' destNum key
+					_ -> return ()
+		modifyState state treeNum srcNum destNum = do
+			gs' <- readIORef gs
+			if (turn gs')
+			then modifyState' state treeNum srcNum destNum
+			else postRedisplay Nothing
+
+		modifyState' state treeList srcNum destNum = do
+			state $~! (+1)
+			state $~! (`mod` 4)
+			resetNums state treeNum srcNum destNum
+			postRedisplay Nothing
+			where
+				resetNums state treeNum srcNum destNum = do
+					state' <- Graphics.UI.GLUT.get state
+					if (state' == 0)
+						then resetNums' treeNum srcNum destNum
+						else return ()
+						where
+							resetNums' treeNum srcNum destNum = do
+								treeNum $~! (*0)
+								srcNum 	$~! (*0)
+								destNum $~! (*0) 
+
+keyboardMouse _ _ _ _ _ _ _ _ _ _ = return ()
+
+vertex4 = Vertex4
+
+reshape size = do
+  viewport $= (Position 0 0, size)
+  postRedisplay Nothing
